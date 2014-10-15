bf85d0ce6034d039d0a4f5da06a3c6925164377a
Olga Shestopalova <olgash@mit.edu>
2014-10-15 12:00:20 -0400

    setup openshift

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var mongoose = require('mongoose');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -14,8 +15,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,6 +23,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+
+// cookies
+app.use(session({ secret: 'beans', 
+                saveUninitialized: true,
+                resave: true }));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -56,5 +61,26 @@ app.use(function(err, req, res, next) {
     });
 });
 
+//  find db string
+var connection_string = 'mongodb://localhost/amazejobs';
+
+if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+    process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+    process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+    process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+    process.env.OPENSHIFT_APP_NAME;
+}
+
+var port = process.env.OPENSHIFT_NODEJS_PORT;
+var ip = process.env.OPENSHIFT_NODEJS_IP;
+
+// connect mongo and go!
+mongoose.connect(connection_string);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.on('open', function () {
+    app.listen(port || 8080, ip);
+});
 
 module.exports = app;
