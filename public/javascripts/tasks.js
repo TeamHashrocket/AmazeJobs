d1e21c05d479039b3279169328a8e985e369a6a4
Catherine Liu <catliu@mit.edu>
2014-10-27 00:42:14 -0400

    fixed delete officially

--- public/javascripts/tasks.js
+++ public/javascripts/tasks.js
@@ -119,6 +119,7 @@ $(document).on('keydown', '#new-task-input', function(event) {
         // this is a delete
         deleteTask(id, function() {
             thisForm.remove();
+            $('[task-id=' + id + ']').remove();
             renderTaskList();
         });
     } else {
