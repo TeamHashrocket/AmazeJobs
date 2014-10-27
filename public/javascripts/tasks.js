05a4fe8be97c8e30a2d6f722b67058843244137e
Elliott Marquez <marquez@mit.edu>
2014-10-27 00:04:35 -0400

    Re-added dynamic sizing? Where did it go?

--- public/javascripts/tasks.js
+++ public/javascripts/tasks.js
@@ -65,7 +65,7 @@ $(document).on('click', '#new-task-label', function(event) {
         year: 'YYYY'
     }));
     $('.ui.dropdown').dropdown();
-    $('.accordion input[type="text"]').keyup(resizeInput);
+    $('.accordion input[type="text"]').keyup(resizeInput).each(resizeInput);
 });
 
 // make a new task or submit an edit
@@ -151,6 +151,7 @@ $(document).on('dblclick', '.task-item .description', function(event) {
     }));
 
     $('.ui.dropdown').dropdown();
+    $('.accordion input[type="text"]').keyup(resizeInput).each(resizeInput);
 });
 
 Handlebars.registerPartial('task', Handlebars.templates['task']);
