bb6622201d0dafe805a27822d127a6ea4cbd5e69
Elliott Marquez <marquez@mit.edu>
2014-10-27 02:08:09 -0400

    Fixed the oauth redirect

--- routes/userController.js
+++ routes/userController.js
@@ -4,11 +4,8 @@ var calendar = google.calendar('v3');
 var plus = google.plus('v1');
 var handleError = require('./utils').handleError;
 var OAuth2 = google.auth.OAuth2;
-// Elliott's personal machine
-// var oauth2Client = new OAuth2('563808076610-dm5h337nmlaq48iktd6crdqqmkba6b0a.apps.googleusercontent.com', 'iFc-NLyc_MVlscfM_ihyHuAb', 'http://tardis.mit.edu:8080/oauthcallback');
-// var oauth2Client = new OAuth2('563808076610-otrdufoeerabtegnb9hua7ac5cuolj1v.apps.googleusercontent.com', 'SVdWatbvETNR87cS4Vg9z4I7', 'https://hakin.mit.edu:8080/oauthcallback');
 // openshift
-var oauth2Client = new OAuth2('563808076610-qk1rp29poub2fpnmdm26tf5n16cd81pl.apps.googleusercontent.com', 's9co0tkklFmv9d2Va47sSXV1', 'http://localhost:8080/oauthcallback');
+var oauth2Client = new OAuth2('563808076610-qk1rp29poub2fpnmdm26tf5n16cd81pl.apps.googleusercontent.com', 's9co0tkklFmv9d2Va47sSXV1', 'http://amazejobs-hashrocket.rhcloud.com/oauthcallback');
 module.exports = {
 
     // login existing users
