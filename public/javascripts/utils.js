72e3487b384ec56076ca437ea4ca527228d7bed4
Catherine Liu <catliu@mit.edu>
2014-10-27 20:22:14 -0400

    changed help dialog to yellow

--- public/javascripts/utils.js
+++ public/javascripts/utils.js
@@ -9,7 +9,20 @@ function removeDialog() {
 }
 
 function showDialog(text) {
-    $('#dialog').html(Handlebars.templates['dialog']({
-        text: text
-    }));
+    
+    if(text == 'Click "Add Task" to add a new task, hit enter to submit. ' +
+                'Double click an existing task to edit it, hit enter to submit. ' +
+                'To delete a task, make its description empty.'){
+		//if it's the initial help text, make the dialog yellow, otherwise make it red
+		$('#dialog').html(Handlebars.templates['dialog']({
+	        text: text,
+	        help:'Help'
+	    }));
+	}else{
+		$('#dialog').html(Handlebars.templates['dialog']({
+        	text: text,
+        	help: ''
+    	}));
+	}
+
 }
